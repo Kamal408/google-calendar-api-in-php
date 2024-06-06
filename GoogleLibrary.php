1dbed03ce6cb8b3c2bb140b1cfc8a95b94523722
Kamal Kafle <kamal.kafle1@gmail.com>
2024-06-06 22:05:17 +0545

    Added google api client package

---
+++ GoogleLibrary.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Central library to access Google APIs Client Library 
+ */
+
+ require_once 'vendor/autoload.php';
+ use Google\Service\Calendar;
+
+class GoogleLibrary
+{
+    public $client;
+
+    function __construct()
+    {
+        $this->client = new Google\Client();
+        $this->client->setAuthConfig('client_secret.json');
+        $this->client->addScope(Calendar::CALENDAR);
+    }
+
+    
+
+
+}
